2ba84a46fcaeb9b2fc57b9a503f34bca280e7427
ETHMUNI <sense1xnft@gmail.com>
2023-04-24 10:22:42 +0200

    first commit

--- Streamingtjeneste/src/Media.java
+++ Streamingtjeneste/src/Media.java
@@ -1,3 +1,12 @@
 public interface Media {
 
+
+    String getTitle(); // The title of the film/series
+
+    int getYear(); // the year when the movie/series first came out
+
+    String getCategory(); // Which category/genre does the film/series belong in
+
+    int getRating(); // The rating of the film and series
+
 }
--- Streamingtjeneste/src/RollingPictures.java
+++ Streamingtjeneste/src/RollingPictures.java
@@ -1,6 +1,19 @@
 public abstract class RollingPictures implements Media {
 
+private String title;
 
+private int year;
 
+private String category;
+
+private int rating;
+
+    public RollingPictures(String title, int year, String category, int rating) {
+        this.title = title;
+        this.year = year;
+        this.category = category;
+        this.rating = rating;
+                
+    }
 }
 
