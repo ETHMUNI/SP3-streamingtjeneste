4aadfecf1fea4dd2a911101616a98435f0d9f7e0
CarolineLykke <carolinelh@live.dk>
2023-04-26 09:21:15 +0200

    hej

--- Streamingtjeneste/src/Main.java
+++ Streamingtjeneste/src/Main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello world!");
+        // Bed brugeren om at indtaste filnavnet og den streng, der skal søges efter
+        System.out.print("Indtast filnavnet: ");
+        String fileName = scanner.nextLine();
+        System.out.print("Indtast søgestrengen: ");
+        String searchString = scanner.nextLine();
 
+        // Kald søgemetoden i FileSearch-klassen
+        Dashboard.search(fileName);
+
+        // Luk scanneren
+        scanner.close();
     }
 }
