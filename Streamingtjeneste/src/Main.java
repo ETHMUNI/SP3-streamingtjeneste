dd426fafed8f0d89ea6320e6fa3524364079a503
ETHMUNI <sense1xnft@gmail.com>
2023-04-27 09:31:29 +0200

    Summary
    Search movies for categories
    Search for all movies
    Play function

--- Streamingtjeneste/src/Main.java
+++ Streamingtjeneste/src/Main.java
@@ -26,21 +26,31 @@ public class Main {
         ////Dashboard
 
         //// Movie Searcher Part
-        Movies movie = new Movies();
-        Scanner scan = new Scanner(System.in);
+        //Movies movie = new Movies();
+        // Scanner scan = new Scanner(System.in);
         String userChoice = textUI.startMenu();
-        String movieUserInput = Movies.getUserInput();
+        // String movieUserInput = Movies.getUserInput();
 
         switch (userChoice) {
             case "1":
-                movie.searchFile(movieUserInput);
+                Movies.searchFile(Movies.getUserInputMovie());
+                Movies.playMovie();
                 break;
             case "2":
-                System.out.println("test");
+                Movies.searchFile(Movies.getUserInputCategory());
+                Movies.playMovie();
                 break;
             case "3":
 
             case "4":
+
+            case "5":
+                // go through the movies.txt and print out all the names
+                // then user enters the name of the movie, user wants to watch
+                Movies.getAllMovies();
+                Movies.playMovie();
+                textUI.startMenu();
+                break;
         }
 
     }
