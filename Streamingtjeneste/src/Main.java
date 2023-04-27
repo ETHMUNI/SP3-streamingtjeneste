0e6dc0220b69ae8289baa23a7b62763756064af2
CarolineLykke <carolinelh@live.dk>
2023-04-27 09:32:29 +0200

    1

--- Streamingtjeneste/src/Main.java
+++ Streamingtjeneste/src/Main.java
@@ -3,10 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         TextUI UI = new TextUI();
-        //FindMovies mo = new FindMovies();
-        FindMovies.Movies mo = new FindMovies.Movies();
-        //UI.startMenu();
-
 
         String userChoice = UI.startMenu();
         switch (userChoice) {
