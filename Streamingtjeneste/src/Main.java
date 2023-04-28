2bd93d5d7b9a6583c35fc21017b642cf5e56a9aa
ETHMUNI <sense1xnft@gmail.com>
2023-04-28 19:00:07 +0200

    Update Main.java

--- Streamingtjeneste/src/Main.java
+++ Streamingtjeneste/src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
 
-    public static MovieHandler csv=new MovieHandler();
-
     public static void main(String[] args) {
         UserMenu userMenu = new UserMenu();
         DashBoard dashBoard = new DashBoard();
